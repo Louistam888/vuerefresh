444f0e49d0bbdfb46275a5ee81a786e18ed333c6
Louistam888 <louistam888@gmail.com>
2025-11-22 16:30:59 -0500

    refresh

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <header>
+    <h1>Quote generator</h1>
+  </header>
+</template>
+
+<style scoped>
+/* HEADER CSS */
+header {
+  color: #ccd6d9;
+  margin-bottom: 25px;
+}
+/**********/
+</style>
